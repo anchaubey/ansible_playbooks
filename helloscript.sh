5a01523117394d0fd7b4652d0e7942bda3a984c2
anchaubey <ankit.chaubey@gmail.com>
2019-12-08 17:15:27 +0000

    adding all files

---
+++ helloscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "read string one: "
+read st1
+
+echo "read string two: "
+read st2
+
+echo ${st1^}
+echo ${st2^^}
+
+
+
---
+++ matches1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+n1=20
+n2=40
+
+echo $( expr $n1 + $n2 )
